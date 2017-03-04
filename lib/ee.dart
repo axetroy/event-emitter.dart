3432d1c09ebd57c438d4d1d6dc02ef12ba553557
axetroy <troy450409405@gmail.com>
2017-03-04 18:13:09 +0800

    feat: set property events readonly now.

--- lib/ee.dart
+++ lib/ee.dart
@@ -1,13 +1,16 @@
 library ee;
+
 class EventEmitter {
 
-  Map<String, List<Function>> events = new Map();
+  Map<String, List<Function>> _events = new Map();
+
+  Map<String, List<Function>> get events => _events;
 
   EventEmitter() {
   }
 
   Function on(String event, void handler(dynamic data)) {
-    final List eventContainer = this.events.putIfAbsent(event, () => new List<Function>());
+    final List eventContainer = _events.putIfAbsent(event, () => new List<Function>());
     eventContainer.add(handler);
     void offThisListener() {
       eventContainer.remove(handler);
@@ -16,7 +19,7 @@ class EventEmitter {
   }
 
   void once(String event, void handler(dynamic data)) {
-    final List eventContainer = this.events.putIfAbsent(event, () => new List<Function>());
+    final List eventContainer = _events.putIfAbsent(event, () => new List<Function>());
     eventContainer.add((dynamic data) {
       handler(data);
       this.off(event);
@@ -24,17 +27,17 @@ class EventEmitter {
   }
 
   void off(String event) {
-    events.remove(event);
+    _events.remove(event);
   }
 
   void emit(String event, [dynamic data]) {
-    final List eventContainer = events[event] ?? [];
+    final List eventContainer = _events[event] ?? [];
     eventContainer.forEach((void handler(dynamic data)) {
       if (handler is Function) handler(data);
     });
   }
 
   void clear() {
-    events.clear();
+    _events.clear();
   }
 }
