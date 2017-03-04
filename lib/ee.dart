91a65df7c0b4d7281267414df338f60385b8a4d7
axetroy <troy450409405@gmail.com>
2017-03-04 18:17:40 +0800

    docs: add comments from Class

--- lib/ee.dart
+++ lib/ee.dart
@@ -2,13 +2,23 @@ library ee;
 
 class EventEmitter {
 
+  /**
+   * storage for the event handler
+   */
   Map<String, List<Function>> _events = new Map();
 
+  /**
+   * proxy the private property, make it readonly. cannot modify from outside
+   */
   Map<String, List<Function>> get events => _events;
 
   EventEmitter() {
   }
 
+  /**
+   * listen the event
+   * it will return a function to cancel this listening
+   */
   Function on(String event, void handler(dynamic data)) {
     final List eventContainer = _events.putIfAbsent(event, () => new List<Function>());
     eventContainer.add(handler);
@@ -18,6 +28,10 @@ class EventEmitter {
     return offThisListener;
   }
 
+  /**
+   * listen the event once
+   * it will remove listening once it trigger
+   */
   void once(String event, void handler(dynamic data)) {
     final List eventContainer = _events.putIfAbsent(event, () => new List<Function>());
     eventContainer.add((dynamic data) {
@@ -26,10 +40,16 @@ class EventEmitter {
     });
   }
 
+  /**
+   * remove a event listening
+   */
   void off(String event) {
     _events.remove(event);
   }
 
+  /**
+   * emit a event with a optional data
+   */
   void emit(String event, [dynamic data]) {
     final List eventContainer = _events[event] ?? [];
     eventContainer.forEach((void handler(dynamic data)) {
@@ -37,6 +57,9 @@ class EventEmitter {
     });
   }
 
+  /**
+   * clear the all listening
+   */
   void clear() {
     _events.clear();
   }
